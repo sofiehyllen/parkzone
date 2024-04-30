13b9673d55ab5e648b07d4df7c5b4cc819ce159d
sofiehyllen <sofiehyllen@hotmail.com>
2024-04-30 18:26:09 +0200

    file relocation

--- strapi/src/api/sondergade17a/routes/sondergade17a.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sondergade17a router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::sondergade17a.sondergade17a');
--- strapi/src/api/sondergade17a/services/sondergade17a.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sondergade17a service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::sondergade17a.sondergade17a');
