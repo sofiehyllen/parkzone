c43647b8a7feff0d821923f7834cc4e5c07b4302
sofiehyllen <sofiehyllen@hotmail.com>
2024-04-30 19:37:07 +0200

    reverse relocate

---
+++ strapi/src/api/sondergade17a/controllers/sondergade17a.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sondergade17a controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sondergade17a.sondergade17a');
